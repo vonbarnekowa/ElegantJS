890f48282af1dc53531fcd91b2aa0095a68e4500
Fralec <Alec@MacBook-Pro-de-Alec.local>
2016-02-10 21:29:56 +0100

    Add js file
    - add elegant function
    - add default variables

---
+++ script/elegant.js
@@ -0,0 +1,25 @@
+(function($){
+    $.fn.elegant = function(options){
+        $.extend({
+            
+            //layout
+            background: 'black',
+            color: 'white',
+            
+            //social
+            facebook: null,
+            twitter: null,
+            linkedin: null,
+            github: null,
+            vk: null,
+            mail: null,
+            website: null,
+            instagram: null,
+            aboutMe: null,
+            flickr: null,
+            googleplus: null,
+            youtube: null
+            
+        }, options);
+    };
+})(jQuery);
