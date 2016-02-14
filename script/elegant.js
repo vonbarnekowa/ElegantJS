5b7e6dd7ede4c41b2c611db5cd7ab3c2a77d1f2e
Fralec <Alec@MacBook-Pro-de-Alec.local>
2016-02-14 15:59:17 +0100

    Change elegantjs
    - optimisation
    - add variables
    - reduce conditions

--- script/elegant.js
+++ script/elegant.js
@@ -1,6 +1,6 @@
 (function($){
     $.fn.elegant = function(opt){
-        var options = $.extend({
+        var options = $.extend(true, {
             
             //config
             newTab: true,
@@ -12,72 +12,105 @@
             iconSize: 1,
             
             //social
-            facebook: null,
-            twitter: null,
-            linkedin: null,
-            github: null,
-            vk: null,
-            mail: null,
-            website: null,
-            instagram: null,
-            flickr: null,
-            googleplus: null,
-            youtube: null
+            facebook: {
+                id: null,
+                faIcon: 'facebook',
+                color: '',
+                url: 'https://www.facebook.com/',
+                target: null},
+                
+            twitter: {
+                id: null,
+                faIcon: 'twitter',
+                color: '',
+                url: 'https://twitter.com/',
+                target: null},
+                
+            linkedin: {
+                id: null,
+                faIcon: 'linkedin',
+                color: '',
+                url: 'https://www.linkedin.com/in/',
+                target: null},
+                
+            github: {
+                id: null,
+                faIcon: 'github',
+                color: '',
+                url: 'https://github.com/',
+                target: null},
+                
+            vk: {
+                id: null,
+                faIcon: 'vk',
+                color: '',
+                url: 'https://vk.com/',
+                target: null},
+                
+            mail: {
+                id: null,
+                faIcon: 'envelope',
+                color: '',
+                url: 'mailto:',
+                target: null},
+                
+            website: {
+                id: null,
+                faIcon: 'link',
+                color: '',
+                url: '',
+                target: null},
+                
+            instagram: {
+                id: null,
+                faIcon: 'instagram',
+                color: '',
+                url: 'https://instagram.com/',
+                target: null},
+                
+            flickr: {
+                id: null,
+                faIcon: 'flickr',
+                color: '',
+                url: 'https://www.flickr.com/photos/',
+                target: null},
+            googleplus: {
+                id: null,
+                faIcon: 'google-plus',
+                color: '',
+                url: 'https://plus.google.com/',
+                target: null},
+            
+            youtube: {
+                id: null,
+                faIcon: 'youtube-play',
+                color: '',
+                url: 'https://youtube.com/',
+                target: null}
             
         }, opt);
         
-
+        var socials = ['facebook', 'twitter', 'linkedin', 'github', 'vk', 'mail', 'website', 'instagram', 'flickr', 'googleplus', 'youtube'];        
+        
         function getNetworks() {
-            var list = "";
-            if(options.newTab) {
-                var target = 'target="_blank"';
+            var list = '';
+            for (var i = 0; i < socials.length; i++) {
+                if(options[socials[i]].id != null) {
+                    var target = null;
+                    if(options.newTab == true) {
+                        target = ' target="_blank"';
+                    } else if(option[socials[i]].target != null) {
+                        target = ' target="' + target + '"';
+                    }
+                    list += '<li id="elg-network elg-' + socials[i] + '">' +
+                            '    <a href="https://www.facebook.com/' + options[socials[i]].id + '"' + target + '>' +
+                            '        <i class="fa fa-' + options[socials[i]].faIcon + ' fa-' + options.iconSize + 'x"></i>' +
+                            '    </a>' +
+                            '</li>';
+                }
             }
             
-            if(options.facebook != null) {
-                list += '<li id="elg-network elg-facebook"><a href="https://www.facebook.com/' + options.facebook + '" ' + target + '><i class="fa fa-facebook fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.twitter != null) {
-                list += '<li id="elg-network elg-twitter"><a href="https://twitter.com/' + options.twitter + '" ' + target + '><i class="fa fa-twitter fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.linkedin != null) {
-                list += '<li id="elg-network elg-linkedin"><a href="https://www.linkedin.com/in/' + options.linkedin + '" ' + target + '><i class="fa fa-linkedin fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.github != null) {
-                list += '<li id="elg-network elg-github"><a href="https://github.com/' + options.github + '" ' + target + '><i class="fa fa-github fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.vk != null) {
-                list += '<li id="elg-network elg-vk"><a href="https://vk.com/' + options.vk + '" ' + target + '><i class="fa fa-vk fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.mail != null) {
-                list += '<li id="elg-network elg-mail"><a href="mailto:' + options.mail + '" ' + target + '><i class="fa fa-envelopegi fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.website != null) {
-                list += '<li id="elg-network elg-website"><a href="' + options.website + '" ' + target + '><i class="fa fa-link fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.instagram != null) {
-                list += '<li id="elg-network elg-instagram"><a href="https://instagram.com/' + options.instagram + '" ' + target + '><i class="fa fa-instagram fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.flickr != null) {
-                list += '<li id="elg-network elg-flickr"><a href="https://www.flickr.com/photos/' + options.flickr + '" ' + target + '><i class="fa fa-flickr fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.googleplus != null) {
-                list += '<li id="elg-network elg-googleplus"><a href="https://plus.google.com/' + options.googleplus + '" ' + target + '><i class="fa fa-google-plus fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            if(options.youtube != null) {
-                list += '<li id="elg-network elg-youtube"><a href="https://youtube.com/' + options.youtube + '" ' + target + '><i class="fa fa-youtube-play fa-' + options.iconSize + 'x"></i></a></li>';
-            }
-            
-            return list
+            return list;
         }
         
         var elegantBody = 
@@ -85,10 +118,13 @@
             '    <div id="elg-model">' +
             '        <span id="elg-cross"><i class="fa fa-times fa-' + options.crossSize + 'x"></i></span>' +
             '        <div id="elg-social">' +
-            '        ' + getNetworks() +
+            '            <ul id="elg-list">' +
+            '                ' + getNetworks() +
+            '            </ul>' +
             '        </div>' +
             '    </div>' +
             '</div>';
+            
         this.parent().append(elegantBody);
     };
 })(jQuery);
